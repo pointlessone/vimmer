69ab6afa0dee00019c37bfe47b79302c7c7a3282
Alexander Mankuta <alex@pointless.one>
2021-06-02 15:21:44 +0300

    Add usernames (1.2)
    Users can specify a username during registration. Usernames must
    follow a certain format and must be unique.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'username' do
+    it 'is unique' do
+      User.create(
+        email: 'u1@example.org',
+        username: 'pure_awesomeness',
+        password: 'passw0rd',
+        password_confirmation: 'passw0rd',
+      )
+
+      user = User.new(
+        email: 'u2@example.org',
+        username: 'pure_awesomeness',
+        password: 'passw0rd',
+        password_confirmation: 'passw0rd',
+      )
+
+      expect(user).to be_invalid
+      expect(user.errors[:username]).to include 'has already been taken'
+    end
+
+    it 'cannot look like an email' do
+      user = User.new(
+        email: 'u1@example.org',
+        username: 'u1@example.org',
+        password: 'passw0rd',
+        password_confirmation: 'passw0rd',
+      )
+
+      expect(user).to be_invalid
+      expect(user.errors[:username]).to include 'is invalid'
+    end
+  end
 end
